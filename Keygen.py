2d3c6cdfe28dead7b75dd6718066b53df6f5db59
Clinksy <clinxyyboo@gmail.com>
2019-12-08 00:08:46 +0200

    gen

---
+++ Keygen.py
@@ -0,0 +1,27 @@
+import random
+import string
+import time
+import os
+import sys
+
+def randomStringDigits(stringLength=30):
+    """Generate a random string of letters and digits """
+    lettersAndDigits = string.ascii_letters + string.digits
+    return ''.join(random.choice(lettersAndDigits) for i in range(stringLength))
+kak =input('generate key?:  ')
+text = randomStringDigits()
+if kak == "yes":
+    print("")
+    print("")
+    print("")
+    print(text)
+    print("")
+    print("")
+    f = open('Keys.txt', 'a')
+    f.write(text + "\n")
+
+os.system('git add Keys.txt')
+os.system("git commit -m 'Update'")
+os.system("git push origin master")
+time.sleep(5)
+
